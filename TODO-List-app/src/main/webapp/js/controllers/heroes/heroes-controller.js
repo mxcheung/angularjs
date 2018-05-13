6b8b6339b6a9ae8abb49be1b4fa69a240c14ee21
max cheung <maxcheung@optusnet.com.au>
2018-05-13 22:19:46 +1000

    Initial setup

--- TODO-List-app/src/main/webapp/js/controllers/heroes/heroes-controller.js
+++ TODO-List-app/src/main/webapp/js/controllers/heroes/heroes-controller.js
@@ -6,7 +6,6 @@ app.controller('HeroesController', function($scope, $http, $location,
 	cat.then(function(response) {
 		$scope.names = response.data;// don't forget "this" in the service
 	})
-	// console.log($scope.names);
 
 	// Setting Now + 5 minutes as default date for datetimepicker.
 	$scope.now = new Date();
@@ -20,32 +19,7 @@ app.controller('HeroesController', function($scope, $http, $location,
 		}, 10);
 
 	};
-	/*
-	 * $scope.screens = [ { scrnid : "1" }, { scrnid : "2" } ]; var table = [];
-	 * $scope.depositSummaryRecords = [ { date : "2018-05-01" }, { date :
-	 * "2018-05-01" } ]; this.depositSummaryRecords =
-	 * $scope.depositSummaryRecords; for ( var date in
-	 * this.depositSummaryRecords) { console.log("date: "); console.log(date);
-	 * console.log(this.depositSummaryRecords[date]); var row = { date : new
-	 * Date("2018-05-01") }; row["aud"] = 1; table.push(row); }
-	 * console.log(table); var students = [ { name : "Mike", track : "track-a",
-	 * achievements : 23, points : 400, }, { name : "james", track : "track-a",
-	 * achievements : 2, points : 21, }, ]
-	 * 
-	 * for ( var key in students) { console.log(students[key]) for ( var track
-	 * in students[key]) { console.log("----"); console.log(track);
-	 * console.log(students[key][track]); } }
-	 * 
-	 * 
-	 * 
-	 * var string1 = ""; var object1 = {a: 1, b: 2, c: 3};
-	 * 
-	 * for (var property1 in object1) {
-	 * 
-	 * console.log("xxx----"); console.log(property1); string1 = string1 +
-	 * object1[property1]; }
-	 * 
-	 */
+
 	var string1 = "";
 	var deposits2 = {
 		"2018-03-01" : {
@@ -63,18 +37,20 @@ app.controller('HeroesController', function($scope, $http, $location,
 	$scope.allCurrencies = [ "aud", "cad", "jpy", "usd" ];
 
 	$scope.loadData = function() {
-		var singleSelectCCY = $scope.singleSelectCCY;
-		$scope.loadDataCCY(singleSelectCCY)
+		$scope.loadDataCCY($scope.singleSelectCCY)
 	};
 
 	$scope.loadDataCCY = function(ccy) {
-		var cashDeposits = HeroesService.getCashDeposits(ccy);
-		cashDeposits
+		HeroesService.getCashDeposits(ccy)
 		.then(function success(response) {
-			$scope.cashDeposits = response.data;// don't forget "this" in the
-			// service
-			let deposits = $scope.cashDeposits;
-			console.log("zzzz----");
+			$scope.table = $scope.tableData(response.data) ;
+		})
+		.catch(function error(rejection) {
+			$scope.error = rejection;
+		});
+	};
+	
+	 $scope.tableData = function(deposits) {
 			var table = [];
 			for ( var date in deposits) {
 				console.log(date);
@@ -87,19 +63,10 @@ app.controller('HeroesController', function($scope, $http, $location,
 				}
 				table.push(row);
 			}
-			console.log(table);
-
-			$scope.table = table;
-		})
-		.catch(function error(rejection) {
-			// this function will be called when the request returned error
-			// status
-		        console.log("error:", rejection); 
-				$scope.dummytext = rejection.status;
-		});
-	};
+         return table;
+     };
 
 	$scope.loadDataCCY("AUD");
-	$scope.dummytext = "hi THERE4";
+	$scope.dummytext = "hi THERE6";
 
 });
