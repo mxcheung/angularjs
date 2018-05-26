cb96d1a01b95f3b2a50710b8875c5f3c7086c071
max cheung <maxcheung@optusnet.com.au>
2018-05-27 08:20:53 +1000

    Add HighChartBarCell

--- WebJar-app/src/main/java/com/maxcheung/models/CellValue.java
+++ WebJar-app/src/main/java/com/maxcheung/models/CellValue.java
@@ -2,6 +2,10 @@ package com.maxcheung.models;
 
 import java.math.BigDecimal;
 
+/*
+ *    see https://google.github.io/guava/releases/23.0/api/docs/com/google/common/collect/Table.Cell.html
+ *    Row key / column key / value triplet corresponding to a mapping in a table.
+ */
 public interface CellValue {
 
 	String getRowKey();
